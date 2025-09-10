8d5f834f9ce354974569f25b2ac43e98b7d346c3
arafat2020 <arafatmannan2019@gmail.com>
2025-09-10 10:38:07 +0600

    MOD: Minor adjustment

--- src/main/java/Producer/ProducerInIt.java
+++ src/main/java/Producer/ProducerInIt.java
@@ -11,6 +11,12 @@ import org.slf4j.LoggerFactory;
 
 public class ProducerInIt {
     public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            runner(i);
+        }
+    }
+
+    private static void runner(int id) {
         Logger logger = LoggerFactory.getLogger(ProducerInIt.class);
         Properties properties = new Properties();
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092");
@@ -18,19 +24,19 @@ public class ProducerInIt {
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
 
-        ProducerRecord<String, String> record = new ProducerRecord<>("Test_Topic", "hello world");
+        ProducerRecord<String, String> record = new ProducerRecord<>("Test_Topic",
+                "hello world" + Integer.toString(id));
         producer.send(record, (metadata, exception) -> {
             if (exception != null) {
-               logger.info("❌ Error sending record: " + exception.getMessage());
+                logger.info("❌ Error sending record: " + exception.getMessage());
             } else {
-               logger.error("✅ Sent to topic " + metadata.topic() +
-                        " partition " + metadata.partition() +
-                        " offset " + metadata.offset());
+                logger.error("✅ Sent to topic \n" + metadata.topic() +
+                        " partition \n" + metadata.partition() +
+                        " offset \n" + metadata.offset());
             }
         });
 
         producer.flush();
         producer.close();
-
     }
 }
