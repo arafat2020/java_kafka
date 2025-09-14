8f3be8e41ca4b27866ccb0ff1405ca4a985c51b9
arafat2020 <arafatmannan2019@gmail.com>
2025-09-14 15:35:13 +0600

    INITIALIZE: Conusmer initialized

---
+++ src/main/java/Consumer/ConsumerInIt.java
@@ -0,0 +1,40 @@
+package Consumer;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ConsumerInIt {
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(ConsumerInIt.class.getName());
+        String server = "localhost:29092";
+        Properties properties = new Properties();
+        String topic = "Test_Topic";
+
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, server);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "earliest");
+
+        // create consumer
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
+        consumer.subscribe(Arrays.asList(topic));
+
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+            for (ConsumerRecord<String, String> record : records) {
+                logger.info("KEY:" + record.key() + ", VALUE:" + record.value());
+                logger.info("Partitions" + record.partition() + ", Offset:" + record.offset());
+            }
+        }
+
+    }
+}
